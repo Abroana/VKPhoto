509ad91d269364b2c1d745f1d1c2cb92b941aea8
Anastasia Abrosimova <abroana19@gmail.com>
2016-10-11 23:57:11 +0300

    Layout improvments

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,7 @@ var Photo = {
 
   Init: function() {
     var parent = this,
-        btn = document.getElementById('load-photo');
+        btn = document.querySelector('.load-photo');
     btn.addEventListener('click', function() {
       VK.init({
         apiId: 5573044
@@ -12,7 +12,7 @@ var Photo = {
 
       VK.Auth.login(function(response) {
         if (response.session) {
-          parent.Model.session = response.session
+          parent.Model.session = response.session;
           parent.Load();
         }
         else
@@ -38,21 +38,30 @@ var Photo = {
     var parent = this,
         data = this.Model.photo,
         user = this.Model.session.user,
-        html ='', i;
-    html += '<div class="main-container__title">' + user.first_name + ' ' + user.last_name + '</div>'
-    html += '<div class="photo-container">'
+        html ='', i,
+        container;
+    container = document.querySelector('.main-container');
+    html += '<div class="main-container__title">' + user.first_name + ' ' + user.last_name + '</div>';
+    html += '<div class="photo-container">';
     for (i = 1; i < data.length; i++) {
-      html += '<div class="photo-container__item">'
-        html += '<img class="photo-container__img" src="' + data[i].src_xbig + '">'
-        html += '<div class="photo-footer">'
-          html += '<img class="photo-footer__img" src="../dist/css/img/heart.png">'
-          html += '<span class="photo-footer__count">' + data[i].likes.count + '</span>'
-        html += '</div>'
-      html += '</div>'
+      html += '<div class="photo-container__item">';
+        html += '<img class="photo-container__img" alt="Фото ' + (i + 1) + '" src="' + data[i].src_big + '" onload="Photo.ImgLoaded(this)" onerror="Photo.ImgErrorLoad(this)">';
+        html += '<div class="photo-footer">';
+          html += '<img class="photo-footer__img" src="../dist/css/img/like.png">';
+          html += '<span class="photo-footer__count">' + data[i].likes.count + '</span>';
+        html += '</div>';
+      html += '</div>';
     }
-    html += '</div>'
-    document.querySelector('.main-container').innerHTML = html;
-    console.log(data);
+    html += '</div>';
+    container.innerHTML = html;
+  },
+
+  ImgLoaded: function(img) {
+    img.parentNode.className += ' photo-container--loaded';
+  },
+
+  ImgErrorLoad: function(img) {
+    img.src = '../dist/css/img/placeholder.png';
   }
 }
 
